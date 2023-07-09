a7c3adb50c00aa61c088d3277fe812171efdafbb
giovanni-a <8248552+giovanni-a@users.noreply.github.com>
2023-07-09 16:49:10 +0200

    Add project files.

---
+++ BlazorApp1/UserControl1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BlazorApp1
+{
+    public partial class UserControl1 : UserControl
+    {
+        readonly ViewModel1 _viewModel = new();
+
+        public UserControl1()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = _viewModel;
+        }
+
+        private void ButtonIncrementCount_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.CurrentCount++;
+        }
+    }
+}
---
+++ BlazorApp1/ViewModel1.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BlazorApp1
+{
+    public class ViewModel1 : INotifyPropertyChanged
+    {
+        public int _currentCount;
+        public int CurrentCount
+        {
+            get => _currentCount;
+            set
+            {
+                if (_currentCount == value) return;
+                _currentCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string callerName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(callerName));
+        }
+    }
+}
